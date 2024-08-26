6697140789f86f0c497302fc046699c04c26b3c8
Thong vongsengdeuan <thonglld@hotmail.com>
2024-08-26 15:33:47 +0700

    Section 12 - Toastr Notifications

---
+++ YumBlazor/Repository/CategoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class CategoryRepository : IcategoryRespository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Category> CreateAsync(Category obj)
+        {
+            await _db.Category.AddAsync(obj);
+            await _db.SaveChangesAsync();
+            return obj;
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var obj = await _db.Category.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj != null) { 
+                _db.Category.Remove(obj);
+                return (await _db.SaveChangesAsync() > 0);
+            }
+            return false;
+        }
+
+        public async Task<Category> GetAsync(int id)
+        {
+            var obj = await _db.Category.FirstOrDefaultAsync(u => u.Id == id);
+            if (obj == null) 
+            { 
+                return new Category();
+            }
+            return obj;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _db.Category.ToListAsync();
+        }
+
+        public async Task<Category> UpdateAsync(Category obj) 
+        {
+            var objFromDb = await _db.Category.FirstOrDefaultAsync(u => u.Id == obj.Id);
+            if(objFromDb is not null)
+            {
+                objFromDb.Name = obj.Name;
+                _db.Category.Update(objFromDb);
+                await _db.SaveChangesAsync();
+                return objFromDb;
+            }
+            return obj;
+        }
+    }
+}
---
+++ YumBlazor/Repository/IRepository/IcategoryRespository.cs
@@ -0,0 +1,13 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface IcategoryRespository
+    {
+        public Task<Category> CreateAsync(Category obj);
+        public Task<Category> UpdateAsync(Category obj);
+        public Task<bool> DeleteAsync(int obj);
+        public Task<Category> GetAsync(int id);
+        public Task<IEnumerable<Category>> GetAllAsync();
+    }
+}
